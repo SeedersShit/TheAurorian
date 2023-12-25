f7ee429576bf1809d905d781e98bdbc9bbf06909
Dimidium <dimidium@dimidium.net>
2023-12-25 20:56:59 +0000

    And so it begins. Upgrade gradle + switch to neoforge

---
+++ src/main/java/gg/seedermodding/theaurorianreforged/TheAurorianReforged.java
@@ -0,0 +1,47 @@
+package gg.seedermodding.theaurorianreforged;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(TheAurorianReforged.MODID)
+public class TheAurorianReforged
+{
+    public static final String MODID = "theaurorianreforged";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public TheAurorianReforged(IEventBus modEventBus)
+    {
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
